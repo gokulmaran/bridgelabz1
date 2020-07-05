ae8ca1d1883ccada36b62b3e0542ce19798519cc
gokulmaran <rmgokulraaj97@gmail.com>
2020-07-05 13:22:05 +0530

    git ignore

---
+++ pattern/RegexSample1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+shopt -s extglob
+echo "enter word ending with thing"
+read word
+pat=[a-zA-Z]{3}
+pat=^[1-9]*
+pat=[1-9a-zA-Z]*$
+pat="^[1-9]*[a-zA-Z]{3}[1-9a-zA-Z]*$"
+if [[ $word =~ $pat ]]
+  then
+       echo yes;
+  else
+       echo no;
+fi
